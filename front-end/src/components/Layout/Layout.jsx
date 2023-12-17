1ceed88fee489520d3fbaf6511d1bca3d9600f68
Przemyslaw Botwina <przemek.botwina@o2.pl>
2023-12-17 11:01:38 +0100

    add Layout children components

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -1,9 +1,14 @@
 import { Footer } from "../Footer/Footer";
+import { MainContent } from "../MainContent/MainContent";
+import { CategoriesMenu } from "../CategoriesMenu/CategoriesMenu";
+
 import styles from "./Layout.module.css";
 
 export function Layout() {
   return (
     <div className={styles.layoutContainer}>
+      <MainContent />
+      <CategoriesMenu />
       <Footer />
     </div>
   );
--- front-end/src/components/MainContent/MainContent.jsx
+++ front-end/src/components/MainContent/MainContent.jsx
@@ -1,7 +1,18 @@
+import { TopBar } from "../TopBar/TopBar";
+import { MainMenu } from "../MainMenu/MainMenu";
+import { Logo } from "../Logo/Logo";
+import { CurrencySelector } from "../CurrencySelector/CurrencySelector";
+import { IconMenu } from "../IconMenu/IconMenu";
+
 export function MainContent() {
   return (
     <>
       <p>MainContent</p>
+      <TopBar />
+      <MainMenu />
+      <Logo />
+      <CurrencySelector />
+      <IconMenu />
     </>
   );
 }
