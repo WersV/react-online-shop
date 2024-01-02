35b7e0e489fedb1aac290e2e9fe868bdc1d17fa6
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-01-02 19:51:10 +0100

    add breadcrumbs styles

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,3 +1,11 @@
+import styles from "./Breadcrumbs.module.css";
+
 export function Breadcrumbs() {
-  return <div>Breadcrumbs</div>;
+  return (
+    <ul className={styles.ulWrapper}>
+      <li>Kobieta</li>
+      <li>Odzież</li>
+      <li className={styles.active}>Swetry</li>
+    </ul>
+  );
 }
